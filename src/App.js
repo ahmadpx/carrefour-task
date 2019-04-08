c0b5c44ee9ac4607f14768d74b81d83724f19630
Ahmed Abdallah <ahmadpx@hotmail.com>
2019-04-08 06:47:39 +0400

    Update app layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Header from './components/Header';
+import SignInForm from './components/SignInForm';
+import Product from './components/Product';
+import { getProducts, searchProducts } from './utils/search.utils';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const Wrapper = styled.div`
+  width: 800px;
+  margin: 0 auto 20px;
+
+  @media (max-width: 840px) {
+    width: 100%;
+    padding: 0 20px;
   }
+`;
+
+const Products = styled.section`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+
+  @media (max-width: 840px) {
+    justify-content: space-around;
+  }
+`;
+
+function App() {
+  const [allProducts] = useState(getProducts());
+  const [query, setQuery] = useState('');
+  const [products, setProducts] = useState(allProducts);
+
+  return (
+    <Wrapper>
+      <Header
+        query={query}
+        onSearch={event => {
+          const query = event.target.value;
+          setQuery(query);
+          setProducts(searchProducts(query, allProducts));
+        }}
+      />
+
+      <SignInForm />
+
+      <Products>
+        {Array.isArray(products) &&
+          products.map((product, index) => (
+            <Product key={index} {...product} />
+          ))}
+      </Products>
+    </Wrapper>
+  );
 }
 
 export default App;
